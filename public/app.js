1c6e11d1728cf29b8d608e96640f9d5b0acce426
Ilya Frolov <absolem.dev@gmail.com>
2023-06-29 14:21:09 +0400

    web server task 2

--- public/app.js
+++ public/app.js
@@ -8,16 +8,27 @@ document.addEventListener("click", (event) => {
   }
 
   if (event.target.dataset.type === "edit") {
-    const id = event.target.dataset.id;
-    const title = prompt(
-      "Введите новое значение",
-      event.target.dataset.title
-    )?.trim();
+    event.target.closest(".note-block").classList.add("d-none");
+    event.target.closest("li").querySelector(".note-edit-block").classList.remove("d-none");
+  }
+
+  if (event.target.dataset.type === "edit-save") {
+    const title = event.target.closest(".note-edit-block").querySelector("input").value.trim();
     if (title) {
-      edit(id, title).then(() => {
-        event.target.parentNode.querySelector("span").innerHTML = title;
+      edit(event.target.dataset.id, title).then(() => {
+        event.target.closest("li").querySelector("span").innerHTML = title;
+        event.target.closest(".note-edit-block").classList.add("d-none");
+        event.target.closest("li").querySelector(".note-block").classList.remove("d-none");
       });
     }
+  }
+
+  if (event.target.dataset.type === "edit-cencel") {
+    event.target.closest(".note-edit-block").classList.add("d-none");
+    event.target.closest("li").querySelector(".note-block").classList.remove("d-none");
+    event.target.closest(".note-edit-block").querySelector("input").value = event.target
+      .closest("li")
+      .querySelector("span").innerText;
   }
 });
 
